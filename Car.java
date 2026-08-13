e6fed1e32473890b7f79fc0c685c5c2679b8e2b7
harshadj5 <harshadj063@gmail.com>
2026-08-13 23:38:35 +0530

    Update file

---
+++ Car.java
@@ -0,0 +1,17 @@
+package Java.OOP;
+// Default constructor
+public class Car {
+    String name;
+    int price;
+
+    Car(){
+        name ="TATA";
+        price =1200000;
+    }
+    public static void main(String[] args) {
+        Car c = new Car();
+        System.out.println("Car Name:"+ c.name);
+        System.out.println("Car Price:"+ price);
+    }
+    
+}
