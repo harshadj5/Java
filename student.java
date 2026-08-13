7b00d0a1e7f652e37ca6d6febf8b2d0f4eb22f33
harshadj5 <harshadj063@gmail.com>
2026-08-13 22:45:45 +0530

    Add parameterized constructor

---
+++ student.java
@@ -0,0 +1,16 @@
+public class student{
+    String name;
+    int rollno;
+    student(String name,int rollno){
+        this.name=name;
+        this.rollno=rollno;
+    }
+    void display(){
+        System.out.println("Name:"+name);
+        System.out.println("Roll no:"+rollno);
+    }
+}
+public static void main(String[]args){
+    student s1 = new student("Amit",12);
+    s1.display();
+}
