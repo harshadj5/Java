2c9925cc364511619748209b7a7617eb5b77d02f
harshadj5 <harshadj063@gmail.com>
2026-08-14 20:23:05 +0530

    Encapsulation example

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Student{
+    private String name;
+
+    // seter method
+    public void setName(String name;){
+        this.name=name;
+    }
+
+    //geter method
+    public String getName(){
+        return name;
+    }
+}
+public class Main{
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("Rajendra");
+        System.out.println(s1.getName());
+    }
+}
+
