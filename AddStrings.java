f0458f535f2c12fa16aaa89046b5e8905c931203
harshadj5 <harshadj063@gmail.com>
2026-08-21 23:02:58 +0530

    Add a string

---
+++ AddStrings.java
@@ -0,0 +1,8 @@
+package Java.String;
+
+public class AddStrings {
+    public static void main(String[] args) {
+        String firstName = "Tony";
+        System.out.println("Full Name:"+firstName);
+    }
+}
